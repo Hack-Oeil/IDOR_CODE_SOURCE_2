9315e07fd99333d7529ef653d473da7ac7ac8e65
Cyrhades <cyrhades76@gmail.com>
2023-11-25 00:41:07 +0100

    Envoi CTF IDOR + code source + hash

---
+++ README.md
@@ -0,0 +1,23 @@
+# CTF - IDOR + COMMENTAIRE CODE SOURCE 2
+
+## Présentation du CTF 
+**ID** 50 dans **les CTFs de Cyrhades**
+
+Dans ce CTF vous devez trouver le mot de passe de l'administrateur et vous connecter avec son compte.
+Mais cette fois le mot de passe est hashé.
+
+
+## Aperçu
+![infos/capture.jpg](infos/capture.jpg)
+
+
+
+-----------
+
+## Installation manuel
+Vous n'utilisez pas l'application **les CTFs de Cyrhades** ? C'est dommage !
+Mais voici comment installer ce CTF manuellement :
+
+> git clone https://github.com/Hack-Oeil/IDOR_CODE_SOURCE_2.git
+
+> cd IDOR_CODE_SOURCE_2
---
+++ db/init.sql/user.sql
@@ -0,0 +1,16 @@
+create table user (
+	id INT,
+	firstname VARCHAR(50),
+	lastname VARCHAR(50),
+	username VARCHAR(50),
+	password VARCHAR(80)
+);
+insert into user (id, firstname, lastname, username, password) values (1, 'Admin', 'Immo', 'idor_admin_bcrypt', '$2y$10$GV3pfKTm7oPTheRh59/Sr.gny/4dyh/ISIPX0jIJ6qHPftxQMAvuK');
+insert into user (id, firstname, lastname, username, password) values (2, 'guest', 'guest', 'guest', '$2y$10$2.B2S8mBsd.VYTS9gGHqqeFIUvlrabHyVZuIOR0dpoonA7XQCVrXi');
+insert into user (id, firstname, lastname, username, password) values (3, 'Admin', '-', 'admin@boutik.com', '$2y$10$4KEzbMkUPAXmmXQlFp07x.3iotUIGvGJwejxn7.d47h84mPnQn./m');
+insert into user (id, firstname, lastname, username, password) values (4, 'Molly', 'Lourens', 'mlourens3@nih.gov', '$2y$10$kab7Ze3JDu/.vkYWusq2m./lfTz3E.xDLslMbNKZiy3Dkvf7o2PDm');
+insert into user (id, firstname, lastname, username, password) values (5, 'Jeanine', 'Carpmile', 'jcarpmile4@github.io', '$2y$10$h6EtlfwWp6kmv.aOFpD/xecjtUeSjqDH/Hle/IzcLx/SADKstN6Ca');
+insert into user (id, firstname, lastname, username, password) values (6, 'Ekaterina', 'Penhearow', 'epenhearow5@dropbox.com', '$2y$10$mvQrEm8C9SVNq6MZKIvvLeTJU7hDjRlkLs1bxvhCJeFI5CjZGbYFK');
+insert into user (id, firstname, lastname, username, password) values (7, 'Wini', 'Gonnelly', 'wgonnelly6@ning.com', '$2y$10$/y8dbhPnRrVrraOZych/7uCX0TNzLHCtDo4nrXb1OEyX1LFwYTlpC');
+insert into user (id, firstname, lastname, username, password) values (8, 'Kimble', 'Alpe', 'kalpe7@ezinearticles.com', '$2y$10$lHlGIvkg6J/9j3kzzGQ3xejhPjOF8iAiNAB9V3RfSuUC6ZVaDMDye');
+insert into user (id, firstname, lastname, username, password) values (9, 'Vonnie', 'Coward', 'vcoward8@fc2.com', '$2y$10$ekCnY1GuiItUfUAt7iPaXeD/SY5U82KcnE3erlv06H.MaOL4KzIte');
